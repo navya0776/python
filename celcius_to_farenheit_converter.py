3d0dd88e5b33dddf27fd7d7e65d3c9e11a6cbe4d
navya0776 <navya0776arora@gmail.com>
2024-09-08 23:01:22 +0530

    Add files via upload

---
+++ celcius_to_farenheit_converter.py
@@ -0,0 +1,6 @@
+def con():
+    b= a*(9/5)+32
+    print(f"{b}°C")
+a=int(input("enter temp in celcius= "))    
+
+con()
---
+++ summation_of_n.py
@@ -0,0 +1,15 @@
+'''def sum(n):
+    if(n==0):
+        return 0
+    return n+sum(n-1)
+n=int(input("enter a nu: "))
+print(sum(n))
+
+sum(n)'''
+
+def fun():
+    print('')
+
+
+ 
+    
